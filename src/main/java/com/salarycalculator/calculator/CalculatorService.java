64665a14123bee72a1bf42e133e828b9bb28a44c
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2018-05-02 00:23:27 +0200

    CalculatorService and CurrencyRateService created
    CalculatorService provides method to calculate salary in PLN, CurrencyRateService provides method to get currency exchange rate from NBP API

--- src/main/java/com/salarycalculator/calculator/CalculatorService.java
+++ src/main/java/com/salarycalculator/calculator/CalculatorService.java
@@ -1,4 +1,37 @@
 package com.salarycalculator.calculator;
 
+import com.salarycalculator.currency.DataRepository;
+import com.salarycalculator.currency.FixedData;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
 public class CalculatorService {
+
+    private static final int DAYS_OF_MONTH = 22;
+    private DataRepository dataRepository;
+    private CurrencyRateService currencyRateService;
+
+    public CalculatorService(DataRepository dataRepository, CurrencyRateService currencyRateService) {
+        this.dataRepository = dataRepository;
+        this.currencyRateService = currencyRateService;
+    }
+
+    public BigDecimal calculateSalaryInPLN(String countryCode, BigDecimal nettSalary) {
+        FixedData dataByCountry = dataRepository.getDataByCountry(countryCode);
+        BigDecimal currencyRate = currencyRateService.getCurrencyRate("USD");
+
+        BigDecimal taxFactor = dataByCountry.getTaxFactor();
+        BigDecimal fixedCost = dataByCountry.getFixedCosts();
+
+        return nettSalary
+                .multiply(new BigDecimal(DAYS_OF_MONTH))
+                .subtract(fixedCost)
+                .multiply(BigDecimal.ONE.subtract(taxFactor))
+                .multiply(currencyRate)
+                .setScale(2, RoundingMode.CEILING);
+
+    }
 }
--- src/main/java/com/salarycalculator/calculator/CurrencyRateService.java
+++ src/main/java/com/salarycalculator/calculator/CurrencyRateService.java
@@ -1,4 +1,20 @@
 package com.salarycalculator.calculator;
 
+import com.salarycalculator.currency.CurrencyRates;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+
+@Service
 public class CurrencyRateService {
+
+    private static final String CURRENCY_RATE_REMOTE_URL = "http://api.nbp.pl/api/exchangerates/rates/a/{currencyCode}/";
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public BigDecimal getCurrencyRate(String currencyCode){
+        CurrencyRates cr = restTemplate.getForObject(CURRENCY_RATE_REMOTE_URL, CurrencyRates.class, currencyCode);
+        return cr.getRates().get(0).getMid();
+    }
 }
