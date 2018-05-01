2f1b28978f8f5eddc08d5aa19698752795439628
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2018-05-02 00:46:09 +0200

    CalculatorController if block added in order to prevent form passing value of salary equal or lower than 0

--- src/main/java/com/salarycalculator/calculator/CalculatorController.java
+++ src/main/java/com/salarycalculator/calculator/CalculatorController.java
@@ -11,13 +11,13 @@ public class CalculatorController {
 
     @RequestMapping(path = "/calculateNettSalaryInPLN/{countryCode}/{nettSalary}", method = RequestMethod.GET)
     public BigDecimal calculateNettSalaryInPLN(@PathVariable String countryCode, @PathVariable BigDecimal nettSalary){
-        return calculatorService.calculateSalaryInPLN(countryCode, nettSalary);
+        if (nettSalary.compareTo(BigDecimal.ZERO) > 0)
+            return calculatorService.calculateSalaryInPLN(countryCode, nettSalary);
+        else
+            return BigDecimal.ZERO;
     }
 
     public CalculatorController(CalculatorService calculatorService){
         this.calculatorService = calculatorService;
     }
-
-    BigDecimal salary = new BigDecimal(1000.33);
-
 }
