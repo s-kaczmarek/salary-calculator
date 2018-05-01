26aae8d693e906259288c37751fc684568cf7820
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2018-05-01 13:02:43 +0200

    TEST
    Reorganizing architecture of application, starting with DDT test.

---
+++ src/test/java/com/salarycalculator/calculator/CalculatorServiceTest.java
@@ -0,0 +1,30 @@
+package com.salarycalculator.calculator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CalculatorServiceTest {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @MockBean
+    private CurrencyRateService currencyRateService;
+
+    @Test
+    public void shouldGetSalaryInPLN(){
+        Mockito.when(currencyRateService.getCurruncyRate(Mockito.any())).thenReturn(new BigDecimal("4"));
+        BigDecimal salaryForGermany = calculatorService.calculateSalaryInPLN("DE", new BigDecimal(1000.00));
+        Assertions.assertThat(salaryForGermany).isEqualTo("67840.00");
+
+}
