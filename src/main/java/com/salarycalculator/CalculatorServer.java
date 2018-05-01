04ac574ee655b35656b2114193d25c1c5788fd28
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2018-05-01 19:12:06 +0200

    CHANGED NAME OF MAIN CLASS
    reorganizing project structure

---
+++ src/main/java/com/salarycalculator/CalculatorServer.java
@@ -0,0 +1,13 @@
+package com.salarycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorServer.class, args);
+    }
+
+}
