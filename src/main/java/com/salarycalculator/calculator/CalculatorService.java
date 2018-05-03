e581005c8c68c8ebde121470d9fae00cf19e8343
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2018-05-03 22:28:50 +0200

    added feature thet returns 0 each time salary value passed is lower or equal 0

--- src/main/java/com/salarycalculator/calculator/CalculatorService.java
+++ src/main/java/com/salarycalculator/calculator/CalculatorService.java
@@ -20,18 +20,24 @@ public class  CalculatorService {
     }
 
     public BigDecimal calculateSalaryInPLN(String countryCode, BigDecimal nettSalary) {
-        FixedData dataByCountry = dataRepository.getDataByCountry(countryCode);
-        BigDecimal currencyRate = currencyRateService.getCurrencyRate("USD");
 
-        BigDecimal taxFactor = dataByCountry.getTaxFactor();
-        BigDecimal fixedCost = dataByCountry.getFixedCosts();
+        if (nettSalary.compareTo(BigDecimal.ZERO) > 0){
 
-        return nettSalary
-                .multiply(new BigDecimal(DAYS_OF_MONTH))
-                .subtract(fixedCost)
-                .multiply(BigDecimal.ONE.subtract(taxFactor))
-                .multiply(currencyRate)
-                .setScale(2, RoundingMode.CEILING);
+            FixedData dataByCountry = dataRepository.getDataByCountry(countryCode);
+            BigDecimal currencyRate = currencyRateService.getCurrencyRate(dataByCountry.getCurrencyCode());
+
+            BigDecimal taxFactor = dataByCountry.getTaxFactor();
+            BigDecimal fixedCost = dataByCountry.getFixedCosts();
+
+            return nettSalary
+                    .multiply(new BigDecimal(DAYS_OF_MONTH))
+                    .multiply(BigDecimal.ONE.subtract(taxFactor))
+                    .subtract(fixedCost)
+                    .multiply(currencyRate)
+                    .setScale(2, RoundingMode.CEILING);
+        }else{
+            return BigDecimal.ZERO.setScale(2, RoundingMode.CEILING);
+        }
 
     }
 }
